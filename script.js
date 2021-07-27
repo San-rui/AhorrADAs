d6685983e80e74d5de8cee1930019b7a85bc169a
San <sruidiaz@hotmail.com>
2021-07-27 10:58:45 +0200

    Setting of the function to go to each section page

---
+++ script.js
@@ -0,0 +1,26 @@
+//--------- NAV BAR BUTTONS--------------------
+var balanceButton = document.querySelector('#balance');
+var categoriesButton = document.querySelector('#categories');
+var reportsButton = document.querySelector('#reports');
+var goToPage = function (event) {
+    var buttonClicked = event.target.value;
+    console.log(buttonClicked);
+    switch (buttonClicked) {
+        case "balance":
+            console.log('Balance');
+            window.location.href = './index.html';
+            break;
+        case "categories":
+            console.log('categories');
+            window.location.href = './categories.html';
+            break;
+        case "reports":
+            console.log('reports');
+            window.location.href = './reports.html';
+            break;
+    }
+    ;
+};
+balanceButton.addEventListener('click', goToPage);
+categoriesButton.addEventListener('click', goToPage);
+reportsButton.addEventListener('click', goToPage);
