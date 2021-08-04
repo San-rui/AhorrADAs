6666b734a3702a78b75b634b78b34184ad19da99
San <sruidiaz@hotmail.com>
2021-08-04 09:43:28 +0200

    add animation

---
+++ assets/animation.js
@@ -0,0 +1,7 @@
+let animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: './assets/lf20_yfdj8maj.json'
+})
