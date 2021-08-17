b42e227eb23afcb938f974b0921b44575e1b885d
CeciTeser <mcteser@gmai.com>
2021-08-17 12:03:55 +0200

    set edit category function

--- assets/categories.ts
+++ assets/categories.ts
@@ -1,5 +1,11 @@
-//------------ ADD ID-------------
+//------------ VARIABLES-------------
+let storage: LocalStorage = goOnStorage();
+const tableCategory = document.getElementById('category-table');
+const addCategoryContainer = document.getElementById('add-category-container');
+const formAddCategory=document.getElementById('form-add-category');
+const nameCat = document.querySelector('#name');
 
+//------------ ADD ID-------------
 const getIdCat = () => {
 
     const storage: LocalStorage = goOnStorage();
@@ -11,13 +17,7 @@ const getIdCat = () => {
 
     return 1;
 }
-
 //------------ ADD CATEGORY-------------
-
-let storage: LocalStorage = goOnStorage();
-const formAddCategory=document.getElementById('form-add-category');
-
-
 const addCategory =(e)=>{
     e.preventDefault();
 
@@ -27,44 +27,49 @@ const addCategory =(e)=>{
 
     const newCategoryName: string = formNewCategory.name.value;
 
-    const newCategoryAdded: NewCategory = {
-            id: getIdCat(),
-            name: newCategoryName,
-            slug: slugify(newCategoryName), 
+    for(const element of storage.categories){
+        if (element.name === newCategoryName){
+            nameCat.value = " ";
+            return alert ('Esta categoría ya existe');
+            
+        } else if (element.name !== newCategoryName){
+            
+            const newCategoryAdded: NewCategory = {
+                id: getIdCat(),
+                name: newCategoryName,
+                slug: slugify(newCategoryName), 
+            }
+
+                storage.categories.push(newCategoryAdded);
+    
+                localStorage.setItem('full-storage', JSON.stringify(storage));
+        
+                nameCat.value = " ";
+
+                return updateTableCategory();
+        }
     }
-
-    storage.categories.push(newCategoryAdded);
-
-    localStorage.setItem('full-storage', JSON.stringify(storage));
-    updateTableCategory();
 
 };
 formAddCategory.addEventListener('submit', addCategory);
 
 //------------SHOW CATEGORY ADDED-------------
-
-const tableCategory = document.getElementById('category-table');
-const addCategoryContainer = document.getElementById('add-category-container');
-const nameCat = document.querySelector('#name');
-
 const updateTableCategory = ()=> {
 
     const storage: LocalStorage = goOnStorage();
 
     tableCategory.innerHTML="";
+
     for(const element of storage.categories){
 
         const newRow= document.createElement('tr');
         const newCategoryNameAdded= document.createElement('td');
         const newRowAction= document.createElement('td');
 
-        newCategoryNameAdded.setAttribute('id', element.slug)
-        
+        newCategoryNameAdded.setAttribute('id', element.slug);
         const editAction =document.createElement('a');
-
         const deleteAction =document.createElement('button');
-        
-        editAction.setAttribute('value', element.slug);
+        editAction.setAttribute('name', element.slug);
         editAction.setAttribute('class','action-class');
         deleteAction.setAttribute('class','action-class');
 
@@ -78,13 +83,10 @@ const updateTableCategory = ()=> {
         newRow.appendChild(newRowAction);
         
         tableCategory.appendChild(newRow);
-
         
         const deleteCategory = () => {
             const newArray= storage.categories.filter(item => element.id !== item.id);
-
             storage.categories=newArray;
-
             localStorage.setItem('full-storage', JSON.stringify(storage));
 
             updateTableCategory();
@@ -92,45 +94,21 @@ const updateTableCategory = ()=> {
 
         deleteAction.addEventListener('click', deleteCategory);
 
-
         const goToEditCategory = (e) => {
-
-
-            const catToEdit= storage.categories.filter(item => element.name === item.name);
-
-            localStorage.setItem('editedElement', JSON.stringify(catToEdit));
-
-            // nameCategory.values = element.name;
-
-            // console.log(nameCategory)
-            // console.log(element.name)
-            
-            // let nameCategorySelected= e.target.value;
-
-            // let selectedElement= document.querySelector(`#${nameCategorySelected}`) ;
-
-            // nameCategory.value= nameCategorySelected;
-
-            // console.log(nameCategorySelected)
-
-            // localStorage.setItem('editedElement', selectedElement.innerHTML);
-
-            // console.log(selectedElement)
+            let nameCategorySelected= e.target.name;
+            let selectedElement= document.querySelector(`#${nameCategorySelected}`) ;
+            localStorage.setItem('editedElement', selectedElement.innerHTML);
 
             const params= new URLSearchParams(window.location.search);
             editAction.setAttribute('href', `./edit-category.html?catId=${element.id}`)
-
         };
 
         editAction.addEventListener('click', goToEditCategory);
-
     }
-
 };
 
 const init = () => {
     updateTableCategory()
 };
-
 init();
 
