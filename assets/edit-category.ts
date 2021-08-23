03a9890350493583ffc05747ceb5b7112deffcd2
San <sruidiaz@hotmail.com>
2021-08-23 11:56:05 +0200

    Fix edit category function to avoid duplicate category

--- assets/edit-category.ts
+++ assets/edit-category.ts
@@ -11,31 +11,37 @@ nameCategory.value = catToEdit;
 
 const editedCategoryName = (e) => {
     e.preventDefault();
-
     let storage: LocalStorage = goOnStorage(); 
 
-    for(const element of storage.categories){
-        if (element.id === nameCategory.value){
+    let myCategoryList=[];
+
+    for ( const element of storage.categories){
+        myCategoryList.push(element.slug);
+    };
+    console.log(myCategoryList);
+    console.log("", nameCategory.value);
+    const slugNameCategory= slugify(nameCategory.value);
+
+        if (myCategoryList.includes(slugNameCategory)){
             nameCategory.value = catToEdit;
+            console.log("soy true")
             return alert ('Esta categoría ya existe');
-            
-        } else if (element.id !== nameCategory.value){
+
+        } else if (!myCategoryList.includes(slugNameCategory)){
             const newArray = storage.categories.map (item =>{
-                
+
                 if(item.name === catToEdit){
                     return {...item, name: nameCategory.value, slug: slugify(nameCategory.value)}
                 }else{
                     return item;
                 }
             });
-        
+
             storage.categories = newArray;
             localStorage.setItem('full-storage', JSON.stringify(storage));
-        
+
             window.location.href="./categories.html"; 
-            
-        }
-    }
+        };
 }
 btnEditCat.addEventListener('click', editedCategoryName);
 
